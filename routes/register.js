a8136b0adcf3407d946090b9b83d3e7414790e01
sohil <sohilgevariya26@gmail.com>
2021-02-03 17:58:03 +0530

    match-mobileno.

--- routes/register.js
+++ routes/register.js
@@ -26,16 +26,25 @@ router.post('/signin', async function(req, res, next) {
         password: password,
         profile: profile
       });
-      record.save();
-        if(record){
-          res.status(200).json({ IsSuccess: true,  Data: record, Message: 'Usser registered' });
-      }else{
-          res.status(200).json({ IsSuccess: false, Data: 0, Message: 'User not registered' });
-      }
+
+        var existphone = await registerSchema.aggregate([{
+          $match: {phone: phone}
+        }]);
+
+        if(existphone){
+        res.status(200).json({ IsSuccess: true, Message: 'mobile no. already registerd' });
+        }
+        else if(record){
+            record.save();
+            res.status(200).json({ IsSuccess: true,  Data: record, Message: 'Usser registered' });
+        }else{
+            res.status(200).json({ IsSuccess: false, Data: 0, Message: 'User not registered' });
+        }
     } catch (error) {
       res.status(500).json({ IsSuccess: false, Message: error.message });
     }
   });
   
+
 
 module.exports = router;
